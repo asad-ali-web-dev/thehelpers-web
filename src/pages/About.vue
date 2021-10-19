3e0743b7b924a69ec3b22401478607baa71e69b2
asad-ali-web-dev <81629007+asad-ali-web-dev@users.noreply.github.com>
2021-10-19 18:01:39 +0500

    added about page and corrected contact page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,3 +1,53 @@
 <template>
-    <h1>About Page</h1>
+    <div class="w-full mt-36">
+
+        <div class="mt-52 mb-20 flex flex-col gap-y-6 justify-center items-center text-center p-8">
+
+            <h1 class="text-6xl text-gray-600">Introduction</h1>
+            <p class="text-2xl font-extralight text-gray-500 max-w-[60ch]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis praesentium corrupti omnis magni harum, nihil iusto. Nostrum, rem. Distinctio magni dolorum similique deserunt quas saepe numquam perspiciatis tempora quis, eius minus dignissimos rem officia dicta officiis impedit quaerat quidem sint laboriosam qui.</p>
+
+        </div>
+
+        <div class="flex flex-col gap-y-12 items-center justify-center mx-auto py-12 bg-gray-100">
+
+            <h1 class="text-4xl font-bold text-gray-800 capitalize">Core Team</h1>
+
+            <div class="flex flex-col lg:flex-row items-center justify-between gap-6 px-12">
+
+                <div v-for="(member, index) in team" :key="index" class="w-auto lg:w-96 flex flex-col gap-y-3 justify-center items-center">
+                    <div class="w-full h-72 bg-red-400"></div>
+                    <h1 class="text-2xl font-medium text-gray-600">{{ member.name }}</h1>
+                    <p>{{ member.description }}</p>
+                    <button class="transition-all duration-700 hover:scale-[1.1] mt-6 rounded-md shadow-xl py-4 px-8 bg-gray-700 hover:bg-helper-yellow text-xl font-bold text-white hover:text-yellow-900">Contact Now</button>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="w-[95%] h-96 mx-auto mt-12 border-4 border-helper-yellow flex flex-col justify-center items-center text-center gap-y-3 p-4">
+            <h1 class="text-5xl text-gray-600">Join Us</h1>
+            <p class="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <div class="relative w-full lg:w-[30rem] h-14 rounded-lg">
+                <input id="email" type="email" class="relative peer w-full h-full px-4 py-3 text-lg text-gray-500 shadow-inner focus:outline-none border-2 border-helper-gray rounded-lg" placeholder=" ">
+                <label for="#email" class="transition-all duration-500 absolute left-3 peer-placeholder-shown:left-5 top-0 -translate-y-1/2 bg-white peer-placeholder-shown:bg-transparent px-2 peer-placeholder-shown:px-0 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 whitespace-nowrap text-md peer-placeholder-shown:text-lg text-gray-800 peer-placeholder-shown:text-gray-500">Your Email</label>
+            </div>
+            <button class="transition-all duration-700 hover:scale-[1.1] mt-6 rounded-md shadow-xl py-4 px-8 bg-gray-700 hover:bg-helper-yellow text-xl font-bold text-white hover:text-yellow-900">Submit</button>
+        </div>
+
+    </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            team: [
+                { name: "lorem ipsem", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum est reiciendis iure facere voluptatibus.", contact: "+920000000000" },
+                { name: "lorem ipsem", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum est reiciendis iure facere voluptatibus.", contact: "+920000000000" },
+                { name: "lorem ipsem", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum est reiciendis iure facere voluptatibus.", contact: "+920000000000" }
+            ]
+        }
+    }
+}
+</script>
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,24 +1,24 @@
 <template>
-    <div class="relative mt-32 w-full h-screen bg-red-400">
-        <div class="absolute inset-0 z-0">
+    <div class="mt-32 w-full h-[calc(100vh-140px)] p-5 mb-[30rem] md:mb-40 lg:mb-0">
+        <div class="hidden lg:block relative lg:absolute lg:inset-0 lg:z-0">
             <iframe class="grayscale" width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Lahore+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
         </div>
-        <div class="absolute top-1/2 right-0 mr-28 -translate-y-1/2 z-10 w-[54rem] h-auto py-12 pl-6 pr-12 bg-white shadow-xl rounded-xl grid grid-cols-5 gap-x-20">
-            <h1 class="capitalize text-4xl font-medium font-montserrat mb-9 col-span-4 pl-6 text-helper-yellow">Start a conversation</h1>
-            <div class="col-span-2 h-full">
+        <div class="relative lg:absolute lg:top-1/2 lg:right-0 lg:mr-28 lg:-translate-y-1/2 lg:z-10 w-full lg:w-[54rem] h-auto lg:py-12 lg:pl-6 lg:pr-12 lg:bg-white lg:shadow-xl lg:rounded-xl grid grid-cols-5 gap-y-10 lg:gap-x-20">
+            <h1 class="capitalize text-xl md:text-4xl font-medium font-montserrat mb-9 col-span-4 pl-6 text-helper-yellow">Start a conversation</h1>
+            <div class="col-span-5 lg:col-span-2 h-full">
                 <div class="p-5 font-montserrat">
-                    <h1 class="text-xl font-medium pb-3 flex flex-row items-center gap-4"> <span class="material-icons">phone</span> <span>Call Now</span></h1>
-                    <p class="font-light text-lg ml-10">+92 303 95 04 808</p>
-                    <p class="font-light text-lg ml-10">+92 302 97 88 441</p>
-                    <p class="font-light text-lg ml-10">+92 305 47 80 626</p>
+                    <h1 class="text-lg md:text-xl font-medium pb-3 flex flex-row items-center gap-4"> <span class="material-icons hidden md:block">phone</span> <span>Call Now</span></h1>
+                    <p class="font-light text-sm md:text-lg ml-0 md:ml-10">+92 303 95 04 808</p>
+                    <p class="font-light text-sm md:text-lg ml-0 md:ml-10">+92 302 97 88 441</p>
+                    <p class="font-light text-sm md:text-lg ml-0 md:ml-10">+92 305 47 80 626</p>
                 </div>
                 <div class="p-5 font-montserrat">
-                    <h1 class="text-xl font-medium pb-3 flex flex-row items-center gap-4"> <span class="material-icons">mail</span> <span>Email Now</span></h1>
-                    <p class="font-light text-lg ml-10">thehelpersociety@gmail.com</p>
-                    <p class="font-light text-lg ml-10">helpersf20@gmail.com</p>
+                    <h1 class="text-lg md:text-xl font-medium pb-3 flex flex-row items-center gap-4"> <span class="material-icons hidden md:block">mail</span> <span>Email Now</span></h1>
+                    <p class="font-light text-sm md:text-lg ml-0 md:ml-10">thehelpersociety@gmail.com</p>
+                    <p class="font-light text-sm md:text-lg ml-0 md:ml-10">helpersf20@gmail.com</p>
                 </div>
             </div>
-            <div class="relative flex flex-col items-end gap-4 col-span-3">
+            <div class="relative flex flex-col items-end gap-4 col-span-5 lg:col-span-3">
                 <div class="relative w-full h-14 rounded-lg">
                     <input id="name" type="text" class="relative peer w-full h-full px-4 py-3 text-lg text-gray-500 shadow-inner focus:outline-none border-2 border-helper-gray rounded-lg" placeholder=" ">
                     <label for="#name" class="transition-all duration-500 absolute left-3 peer-placeholder-shown:left-5 top-0 -translate-y-1/2 bg-white peer-placeholder-shown:bg-transparent px-2 peer-placeholder-shown:px-0 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 whitespace-nowrap text-md peer-placeholder-shown:text-lg text-gray-800 peer-placeholder-shown:text-gray-500">Your Name</label>
